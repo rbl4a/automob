67179f0d12962b6dda938ef982c47a4ee6650bf4
rbl4a <rbl4a@icloud.com>
2021-04-09 12:23:20 +0500

    impl some ParkingRepository method

--- src/main/java/com/example/demo_console/controller/ParkingController.java
+++ src/main/java/com/example/demo_console/controller/ParkingController.java
@@ -3,7 +3,6 @@ package com.example.demo_console.controller;
 import com.example.demo_console.entity.Parking;
 import com.example.demo_console.service.ParkingService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
